bebfe7f8562df9006c5663ea2271f059e480d62f
Antipoison <orders.connor@gmail.com>
2025-08-20 23:21:38 +1000

    Rewrote the checking code to stop sucking - Checking to see if damage is greater than the last     - That's just not how the Payday functions work + Moved checking to occur within RunHttpRequest     + This is because it made the docs unmaintainable       as I would have to update them every time i       changed the code, I don't have to do that now + docs     + Added examples for people who are not familiar       with URL Encoding for GET requests     = Still have not figured out POST requests       If someone knows, please tell me

--- DamageHook/lua/damage_ping.lua
+++ DamageHook/lua/damage_ping.lua
@@ -1,16 +1,30 @@
 if not _G.DamagePingMod then
     _G.DamagePingMod = {}
     DamagePingMod.path = ModPath
-    DamagePingMod.last_damage = 0
     DamagePingMod.last_trigger_time = 0
     DamagePingMod.cooldown = 0.3 -- Cooldown to ensure not spamming the server, may drop damage if graced
 end
 
 -- Function to ping the specified server using a get request
-function DamagePingMod:RunHttpRequest()
-    dohttpreq("http://localhost:1327", function(data, id)
-		log("Retrieved server data:\n" .. data)
-	end)
+function DamagePingMod:RunHttpRequest(attack_data, ...)
+    -- Check if the player took damage and it's a new damage event
+    if attack_data and attack_data.damage and attack_data.damage > 0 then
+        local current_time = os.clock()
+        -- Only trigger once within the cooldown
+        if current_time - DamagePingMod.last_trigger_time >= DamagePingMod.cooldown then
+
+            -- Please note, special characters need to be encoded using percentage encoding https://www.w3schools.com/tags/ref_urlencode.ASP
+            -- Example
+                -- "https://localhost:1327?chains=In%20A%20%Pickle"
+            -- For further reference on passing information using GET, read https://www.w3schools.com/tags/ref_httpmethods.asp
+            dohttpreq("http://localhost:1327", function(data, id)
+                log("Retrieved server data:\n" .. data)
+            end)
+
+            DamagePingMod.last_trigger_time = current_time
+            DamagePingMod.last_damage = attack_data.damage
+        end
+    end
 end
 
 -- Hook into PlayerDamage:damage_bullet to trigger a function when player is shot
@@ -18,19 +32,7 @@ local original_damage_bullet = PlayerDamage.damage_bullet
 function PlayerDamage:damage_bullet(attack_data, ...)
     local result = original_damage_bullet(self, attack_data, ...)
     
-    -- Check if the player took damage and it's a new damage event
-    if attack_data and attack_data.damage and attack_data.damage > 0 then
-        local current_time = os.clock()
-        -- Only trigger once within the cooldown
-        if current_time - DamagePingMod.last_trigger_time >= DamagePingMod.cooldown and attack_data.damage <= DamagePingMod.last_damage then
-            DamagePingMod:RunHttpRequest()
-            DamagePingMod.last_trigger_time = current_time
-            DamagePingMod.last_damage = attack_data.damage
-        elseif attack_data.damage > DamagePingMod.last_damage then
-            -- Update last damage if current damage is higher (new damage event)
-            DamagePingMod.last_damage = attack_data.damage
-        end
-    end
+    DamagePingMod:RunHttpRequest(attack_data)
     
     return result
 end
@@ -40,19 +42,7 @@ local original_damage_melee = PlayerDamage.damage_melee
 function PlayerDamage:damage_melee(attack_data, ...)
     local result = original_damage_melee(self, attack_data, ...)
     
-    -- Check if the player took damage and it's a new damage event
-    if attack_data and attack_data.damage and attack_data.damage > 0 then
-        local current_time = os.clock()
-        -- Only trigger once within the cooldown
-        if current_time - DamagePingMod.last_trigger_time >= DamagePingMod.cooldown and attack_data.damage <= DamagePingMod.last_damage then
-            DamagePingMod:RunHttpRequest()
-            DamagePingMod.last_trigger_time = current_time
-            DamagePingMod.last_damage = attack_data.damage
-        elseif attack_data.damage > DamagePingMod.last_damage then
-            -- Update last damage if current damage is higher (new damage event)
-            DamagePingMod.last_damage = attack_data.damage
-        end
-    end
+    DamagePingMod:RunHttpRequest(attack_data)
     
     return result
 end
